01d4305b97fb782516e5912d9aaca78a06b91f80
Sam Jeeves <sam.jeeves@realworld-one.com>
2020-11-04 18:10:08 +0100

    More fix

--- src/prepare.js
+++ src/prepare.js
@@ -2,6 +2,6 @@ const ver = require('react-native-version')
 module.exports = async (pluginConfig, context) => {
   const { logger } = context;
   logger.log("Setting iOS and Android version.");
-  const versionResult = await ver.version(projectPath = '.');
+  const versionResult = await ver.version({args: ['.']}, '.');
   logger.log(versionResult);
 };
